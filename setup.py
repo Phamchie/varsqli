785221db6d0f5a040168a3eb1cdeca277666cba9
Phamchie <97181079+Phamchie@users.noreply.github.com>
2023-07-27 16:50:02 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+import os
+import time
+print("[INFO] Starting Setup Tools...")
+os.system('pip3 install lxml')
+print('[INFO] find 1 requirements : lxml')
+time.sleep(0.30)
+print('[INFO] starting setup colorama')
+os.system('pip3 install colorama')
+print('[INFO] Find 2 Requirements : colorama')
+time.sleep(0.20)
+print('[+] Ok , Command : python3 varsqli.py')
+exit()

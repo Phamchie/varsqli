4c333cb657acc699616e4899016814752e47e84a
Pham Chien <97181079+Phamchie@users.noreply.github.com>
2023-08-03 16:21:10 +0700

    Update varsqli.py

--- varsqli.py
+++ varsqli.py
@@ -149,7 +149,6 @@ if target_url:
                         count_num += 1
                         results = requests.get(target_url + payload)
                         if results.status_code == 200:
-                            print("[INFO] Total Response Content HTML stable : 200 OK")
                             if "The used SELECT" and "The used SELECT statements have a different number of columns" in results.text:
                                 pass
                             else:
